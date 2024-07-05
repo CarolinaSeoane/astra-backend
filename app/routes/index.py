512108022600fbfb2344b95931a678fa6a1309c7
Belen Seoane <seoane.m.b@gmail.com>
2024-07-05 19:16:14 -0300

    fixed str

--- app/routes/index.py
+++ app/routes/index.py
@@ -6,6 +6,6 @@ index = Blueprint("index", __name__)
 
 @index.route("/")
 def init_teams():
-    print(f"in the route {os.environ.get("FLASK_APP")}")
+    print(f"in the route {os.environ.get('FLASK_APP')}")
 
     return jsonify("Hello World (from the index page!)", 200)
