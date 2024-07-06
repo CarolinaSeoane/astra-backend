d80508e611e5115d0a6e12c82287249023c57e75
CarolinaSeoane <carolina.b.seoane@gmail.com>
2024-07-05 22:27:22 -0300

    Fix json response

--- app/routes/teams.py
+++ app/routes/teams.py
@@ -9,6 +9,9 @@ teams = Blueprint("teams", __name__)
 @teams.route("/")
 def init_teams():
     list_cursor = list(mongo.db.movies.find(limit = 5))    
-    response = json_util.dumps(list_cursor)    
-    return response
+    response = json_util.dumps(list_cursor) 
+    # print(type(json.loads(response)[0])) We are returning a JSON a list of <class 'dict'>
+    response = json.loads(response)
+
+    return jsonify(response)
 
