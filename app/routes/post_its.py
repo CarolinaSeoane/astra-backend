5f245d790d1a58ee1d12d206d3a67c1ef5f1ad08
Matmass99 <matucho-99@hotmail.com>
2024-08-16 12:30:04 -0300

    Updated

--- app/routes/post_its.py
+++ app/routes/post_its.py
@@ -1,13 +1,24 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, g, request, jsonify
 from webargs.flaskparser import use_args
 from webargs import fields
 from app.models.post_it import PostIt
-from app.db_connection import mongo  
+from app.db_connection import mongo
+from app.routes.utils import validate_user_is_member_of_team  
 
 post_its = Blueprint("post_its", __name__)
 
+excluded_routes = []
+
+@post_its.before_request
+def apply_validate_user_is_member_of_team():
+    for route in excluded_routes:
+        if request.path.startswith(route):
+            return None
+
+    return validate_user_is_member_of_team()
+
 @post_its.route("/", methods=['GET'])
-@use_args({'sprint_id': fields.Str(required=True),}, location='query')
+@use_args({'sprint_id': fields.Str(required=True)}, location='query')
 def get_post_its(args):
     sprint_id = args.get('sprint_id')
     
@@ -17,7 +28,7 @@ def get_post_its(args):
     return jsonify(post_its)
 
 @post_its.route("/", methods=['POST'])
-@use_args({'content': fields.Str(required=True),'sprint_id': fields.Str(required=True),}, location='json')
+@use_args({'content': fields.Str(required=True), 'sprint_id': fields.Str(required=True)}, location='json')
 def add_post_it(args):
     new_post_it = PostIt(content=args['content'], sprint_id=args['sprint_id'])
     
