ce8a447caa0913fbc173ad2c44cd57fe5eec3c09
CarolinaSeoane <carolina.b.seoane@gmail.com>
2024-10-08 00:51:01 -0300

    H0026 schedule ceremonies part II

--- app/models/ceremony.py
+++ app/models/ceremony.py
@@ -16,46 +16,68 @@ class Ceremony:
         # google data
 
     def create_sprint_ceremonies(team_id):
-        team_ceremonies_settings = Team.get_team_settings(team_id, 'ceremonies')
+        team_ceremonies_settings = Team.get_team_settings(team_id, 'ceremonies')['ceremonies']
         sprint_set_up = Team.get_team_settings(team_id, 'sprint_set_up')['sprint_set_up']
         generate_sprint_schedule(team_ceremonies_settings, sprint_set_up)
         
 
 # def generate_sprint_schedule(ceremonies, sprint_set_up, sprint_start_date=None):
 def generate_sprint_schedule(team_ceremonies_settings, sprint_set_up, sprint_start_date=None):
-        schedule = []
+    schedule = []
 
-        # Determine sprint start date
-        if sprint_start_date:
-            sprint_start = datetime.strptime(sprint_start_date, "%Y-%m-%d")
-        else:
-            # Assuming the sprint starts on the next specified weekday from today
-            today = datetime.today()
-            sprint_start_weekday = get_weekday_number(sprint_set_up['sprint_begins_on'])
-            sprint_start = get_next_weekday(today, sprint_start_weekday)
-        
-        # Determine sprint end date
-        sprint_end = sprint_start + timedelta(weeks=sprint_set_up['sprint_duration']) - timedelta(days=1)
+    # Determine sprint start date
+    if sprint_start_date:
+        sprint_start = datetime.strptime(sprint_start_date, "%Y-%m-%d")
+    else:
+        # Assuming the sprint starts on the next specified weekday from today
+        today = datetime.today()
+        sprint_start_weekday = get_weekday_number(sprint_set_up['sprint_begins_on'])
+        sprint_start = get_next_weekday(today, sprint_start_weekday)
     
-        # Schedule planning ceremony
-        planning = team_ceremonies_settings['ceremonies']['planning']
-        if planning['when'] == CeremonyStartOptions.BEGINNING.value:
-            planning_start_time = planning['starts']
-            planning_end_time = planning['ends']
-           
-            # Schedule on sprint_start date
-            planning_datetime_start = datetime.combine(sprint_start, time(hour=int(planning_start_time.split(":")[0]), minute=int(planning_start_time.split(":")[1])))
-            planning_datetime_end = datetime.combine(sprint_end, time(hour=int(planning_end_time.split(":")[0]), minute=int(planning_end_time.split(":")[1])))
+    # Determine sprint end date
+    sprint_end = sprint_start + timedelta(weeks=sprint_set_up['sprint_duration']) - timedelta(days=1)
 
-            schedule.append({
-                'type': CeremonyType.PLANNING.value,
-                'starts': planning_datetime_start,
-                'ends': planning_datetime_end,
-                'google_meet_config': planning['google_meet_config'],
-                'status': CeremonyStatus.NOT_HAPPENED_YET.value,
-                # 'meeting_code': 'PlanningS32024',
-                # 'date': 'Mon 05/03',
-                # 'duration': '1 hr 30 mins',
-                # 'team_members': team_members
-            })
+    # Schedule planning ceremony
+    planning = generate_planning_ceremony(team_ceremonies_settings['planning'], sprint_start, sprint_end)
+    schedule.append(planning)
+
+    # Schedule standup ceremonies
+    standups = generate_standup_ceremonies(team_ceremonies_settings['standup'], sprint_start, sprint_end)
+    schedule.extend(standups)
+
+    # Schedule retrospective ceremony
+    retro = generate_retrospective_ceremony(team_ceremonies_settings['retrospective'], sprint_start, sprint_end)
+    schedule.append(retro)
+      
+def generate_planning_ceremony(team_planning_settings, sprint_start_date, sprint_end_date):
+    planning_start_time = team_planning_settings['starts']
+    planning_end_time = team_planning_settings['ends']
+
+    if team_planning_settings['when'] == CeremonyStartOptions.BEGINNING.value:
+        # Schedule on sprint_start date
+        start_date = sprint_start_date
+    else:
+        # Schedule on sprint_end date
+        start_date = sprint_end_date
+
+    planning_datetime_start = datetime.combine(start_date, time(hour=int(planning_start_time.split(":")[0]), minute=int(planning_start_time.split(":")[1])))
+    planning_datetime_end = datetime.combine(start_date, time(hour=int(planning_end_time.split(":")[0]), minute=int(planning_end_time.split(":")[1])))
+    
+    return {
+        'type': CeremonyType.PLANNING.value,
+        'starts': planning_datetime_start,
+        'ends': planning_datetime_end,
+        'google_meet_config': team_planning_settings['google_meet_config'],
+        'status': CeremonyStatus.NOT_HAPPENED_YET.value,
+        # 'meeting_code': 'PlanningS32024',
+        # 'date': 'Mon 05/03',
+        # 'duration': '1 hr 30 mins',
+        # 'team_members': team_members
+    }
+
+def generate_standup_ceremonies(team_standup_settings, sprint_start_date, sprint_end_date):
+    pass
+
+def generate_retrospective_ceremony(team_retro_settings, sprint_start_date, sprint_end_date):
+    pass
     
